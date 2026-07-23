005fd4ecec3d9f1ac5217945f48545c28243602b
Aqil-Ahmad <aaqilahmadd@gmail.com>
2026-07-23 21:27:21 +0500

    migration update

--- exhibition/migrations/0010_exhibitorsettings_call_private_and_more.py
+++ exhibition/migrations/0010_exhibitorsettings_call_private_and_more.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("exhibition", "0008_exhibitionemailqueue"),
+        ("exhibition", "0009_exhibitorinfo_active"),
     ]
 
     operations = [
