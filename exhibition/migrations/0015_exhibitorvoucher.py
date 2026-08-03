22d28b6d283fcf9ca0140d0af7734ee096a839cd
Aqil-Ahmad <aaqilahmadd@gmail.com>
2026-08-03 14:40:08 +0500

    chore: update migration

--- exhibition/migrations/0015_exhibitorvoucher.py
+++ exhibition/migrations/0015_exhibitorvoucher.py
@@ -5,7 +5,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
     dependencies = [
         ("base", "0001_initial"),
-        ("exhibition", "0013_exhibitionemailqueue_scheduled_at"),
+        ("exhibition", "0014_exhibitionproposal_profile_edited_at"),
     ]
 
     operations = [
