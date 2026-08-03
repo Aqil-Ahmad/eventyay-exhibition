11cfba0a5c66c2b0a2b76412ad9ceb04134af565
Aqil-Ahmad <aaqilahmadd@gmail.com>
2026-08-03 13:13:55 +0500

    fix newly added tests

--- tests/test_accepted_proposal_edit.py
+++ tests/test_accepted_proposal_edit.py
@@ -51,6 +51,7 @@ def test_draft_action_is_ignored_by_get_form_kwargs_when_accepted(event):
         proposal, _ = _accepted_proposal(event)
         request = RequestFactory().post("/", data={"action": "draft", "name_0": "Acme", "email": ""})
         request.user = proposal.user
+        request.event = event
         view = _edit_view(proposal, request)
         assert view.get_form_kwargs()["draft_save"] is False
 
@@ -62,6 +63,7 @@ def test_crafted_draft_action_cannot_bypass_required_field_validation(event):
         proposal, exhibitor = _accepted_proposal(event)
         request = RequestFactory().post("/", data={"action": "draft", "name_0": "Acme", "email": ""})
         request.user = proposal.user
+        request.event = event
 
         view = _edit_view(proposal, request)
         form = ExhibitionProposalForm(**view.get_form_kwargs())
@@ -86,6 +88,7 @@ def test_form_valid_syncs_accepted_proposal_and_marks_edited(event):
         )
         request.user = proposal.user
         request.event = event
+        request.session = {}
         setattr(request, "_messages", FallbackStorage(request))
 
         view = _edit_view(proposal, request)
