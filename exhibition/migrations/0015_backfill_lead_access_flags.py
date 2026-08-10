7155cf16c9263d5fccf1a5c1a1d0b83e48c3b3a7
Aqil Ahmad <aaqilahmadd@gmail.com>
2026-08-10 14:23:15 +0500

    Merge pull request #96 from Aqil-Ahmad/leadnvocher
    Implement lead-access enforcement on all endpoints

---
+++ exhibition/migrations/0015_backfill_lead_access_flags.py
@@ -0,0 +1,19 @@
+from django.db import migrations
+
+
+def backfill_lead_flags(apps, schema_editor):
+    ExhibitorInfo = apps.get_model("exhibition", "ExhibitorInfo")
+    ExhibitorInfo.objects.filter(is_exhibitor=True).update(
+        lead_scanning_enabled=True,
+        allow_lead_access=True,
+    )
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("exhibition", "0014_exhibitionproposal_profile_edited_at"),
+    ]
+
+    operations = [
+        migrations.RunPython(backfill_lead_flags, migrations.RunPython.noop),
+    ]
