295259ac55b9c9c7c43b41d00fa56e7a662b9d77
Aqil-Ahmad <aaqilahmadd@gmail.com>
2026-08-16 14:01:20 +0500

    fix: RTL locale preview shown LTR

--- exhibition/templatetags/exhibition_email.py
+++ exhibition/templatetags/exhibition_email.py
@@ -1,4 +1,5 @@
 from django import template
+from django.conf import settings
 
 from ..models import ExhibitionEmailQueue
 
@@ -9,3 +10,9 @@ register = template.Library()
 def pending_email_count(event):
     """Number of unsent queued emails for the event, shown as a nav badge."""
     return ExhibitionEmailQueue.objects.filter(event=event, sent_at__isnull=True).count()
+
+
+@register.filter
+def locale_dir(locale):
+    """``rtl`` for bidirectional locales, matching the i18n editor widgets."""
+    return "rtl" if str(locale).split("-")[0] in settings.LANGUAGES_BIDI else "ltr"
