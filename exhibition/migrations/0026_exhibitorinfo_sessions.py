fda49a2820258aa7dca130e770a1d96a860be77f
Aqil-Ahmad <aaqilahmadd@gmail.com>
2026-09-10 18:01:04 +0500

    fix migration numbering

--- exhibition/migrations/0026_exhibitorinfo_sessions.py
+++ exhibition/migrations/0026_exhibitorinfo_sessions.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
     dependencies = [
         ("base", "0001_initial"),
-        ("exhibition", "0024_voucher_email_attachment"),
+        ("exhibition", "0025_voucher_pools"),
     ]
 
     operations = [
