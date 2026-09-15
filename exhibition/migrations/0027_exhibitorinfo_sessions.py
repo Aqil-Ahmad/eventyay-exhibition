baba6127df378abaabe2297c84aa5d9c1c7742fc
Aqil-Ahmad <aaqilahmadd@gmail.com>
2026-09-15 12:56:36 +0500

    fix migration

--- exhibition/migrations/0027_exhibitorinfo_sessions.py
+++ exhibition/migrations/0027_exhibitorinfo_sessions.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
     dependencies = [
         ("base", "0001_initial"),
-        ("exhibition", "0025_voucher_pools"),
+        ("exhibition", "0026_device_default_count"),
     ]
 
     operations = [
